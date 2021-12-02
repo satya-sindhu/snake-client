6a755c3d1b2ba5dd57947c2debade78ade903ee7
satya sindhu <movva001@gmail.com>
2021-12-01 19:15:46 -0500

    wasd movement

--- play.js
+++ play.js
@@ -1,23 +1,48 @@
 const net = require("net");
 const {IP, PORT} = require('./constants');
 
+// setup interface to handle user input from stdin
+
+// const setupInput = function (stdin.on("data", handleUserInput);) {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   return stdin;
+// };
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: "50541"
+    // host: "165.227.47.243",
+    // port: "50541"
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   console.log("hello Satya welcome to the game!");
+
   conn.on('connect', () => {
+
     conn.write('Name: SATYA');
     conn.write('Say: Hi');
+    // conn.write('Move: up'); 
+    // conn.write('Move: left');
+    // conn.write('Move: down');
+    // conn.write('Move: right');
+    //setTimeout(() => {conn.write('Move: up');}, 500);   
+    //setTimeout(() => {conn.write('Move: left');}, 500);   
+    //setTimeout(() => {conn.write('Move: down');}, 500);   
+    //setTimeout(() => {conn.write('Move: right');}, 500);  
+    
+    //setInterval(() => {conn.write('Move: up');}, 500); 
+    //setInterval(() => {conn.write('Move: left');}, 500); 
+    //setInterval(() => {conn.write('Move: down');}, 500); 
+    //setInterval(() => {conn.write('Move: right');}, 500); 
   })
+  // interpret incoming data as text
 
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
+module.exports = connect;
